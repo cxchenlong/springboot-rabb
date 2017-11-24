c4193c051d99173c4f5a6e293ba7e8572a05a2ef
Your Name <email@example.com>
2017-11-24 19:01:30 +0800

    9898

---
+++ src/main/java/com/jk/controller/rabbia/object/ObjectReceiver.java
@@ -0,0 +1,17 @@
+package com.jk.controller.rabbia.object;
+
+import com.jk.controller.model.User;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "object")
+public class ObjectReceiver {
+
+    @RabbitHandler
+    public void process(User user) {
+        System.out.println("Receiver object : " + user);
+    }
+
+}
